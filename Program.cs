2cae82aba79c3728107d164393daa5f3d4454013
Diego Neri <diego@diegoneri.com>
2023-10-06 20:27:58 -0300

    Adiciona ToUpper para as entradas

--- Program.cs
+++ Program.cs
@@ -6,16 +6,19 @@ string Deveria = "";
 Console.WriteLine("Está se movendo?");
 EstaSeMovendo = Console.ReadLine()!.ToUpper();
 
-Console.WriteLine("Deveria?");
-Deveria = Console.ReadLine()!.ToUpper();
-
 if (EstaSeMovendo == RESPOSTA_SIM){
+    Console.WriteLine("Deveria?");
+    Deveria = Console.ReadLine()!;
+
     if (Deveria == RESPOSTA_SIM){
         Console.WriteLine("ÓTIMO");
     }else{
         Console.WriteLine("USE SILVER TAPE");
     }
 }else{
+    Console.WriteLine("Deveria?");
+    Deveria = Console.ReadLine()!.ToUpper();
+
     if (Deveria == RESPOSTA_SIM){
         Console.WriteLine("USE WD-40");
     }else{
