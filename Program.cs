930c086609364f8019eed8af8d346ea3811fd71e
Diego Neri <diego@diegoneri.com>
2023-10-06 20:46:54 -0300

    Finaliza programa

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿const string RESPOSTA_SIM = "SIM";
+const string RESPOSTA_NAO = "NÃO";
 
 string EstaSeMovendo = "";
 string Deveria = "";
@@ -8,16 +9,14 @@ EstaSeMovendo = Console.ReadLine()!.ToUpper();
 
 Console.WriteLine("Deveria?");
 Deveria = Console.ReadLine()!.ToUpper();
-if (EstaSeMovendo == RESPOSTA_SIM){
-    if (Deveria == RESPOSTA_SIM){
-        Console.WriteLine("ÓTIMO");
-    }else{
-        Console.WriteLine("USE SILVER TAPE");
-    }
+
+bool ehWD = EstaSeMovendo == RESPOSTA_NAO && Deveria == RESPOSTA_SIM;
+bool ehSilverTape = EstaSeMovendo == RESPOSTA_SIM && Deveria == RESPOSTA_NAO;
+
+if (ehWD){
+    Console.WriteLine("USE WD-40");
+}else if(ehSilverTape){
+    Console.WriteLine("USE SILVER TAPE");
 }else{
-    if (Deveria == RESPOSTA_SIM){
-        Console.WriteLine("USE WD-40");
-    }else{
-        Console.WriteLine("ÓTIMO");
-    }
+    Console.WriteLine("ÓTIMO");
 }
