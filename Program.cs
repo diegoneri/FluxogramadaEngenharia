ad633b5f2efc8e5d0401a5f587ae36ead6dee4ff
Diego Neri <diego@diegoneri.com>
2023-10-06 20:30:35 -0300

    Refatora locais da segunda pergunta

--- Program.cs
+++ Program.cs
@@ -6,19 +6,15 @@ string Deveria = "";
 Console.WriteLine("Está se movendo?");
 EstaSeMovendo = Console.ReadLine()!.ToUpper();
 
+Console.WriteLine("Deveria?");
+Deveria = Console.ReadLine()!.ToUpper();
 if (EstaSeMovendo == RESPOSTA_SIM){
-    Console.WriteLine("Deveria?");
-    Deveria = Console.ReadLine()!.ToUpper();
-
     if (Deveria == RESPOSTA_SIM){
         Console.WriteLine("ÓTIMO");
     }else{
         Console.WriteLine("USE SILVER TAPE");
     }
 }else{
-    Console.WriteLine("Deveria?");
-    Deveria = Console.ReadLine()!.ToUpper();
-
     if (Deveria == RESPOSTA_SIM){
         Console.WriteLine("USE WD-40");
     }else{
